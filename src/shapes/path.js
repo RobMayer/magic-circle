3af84aad4dfdb295e2fe1541f48f26e892d643ba
RobMayer <5171361+RobMayer@users.noreply.github.com>
2021-05-16 21:37:47 -0400

    typo

--- src/shapes/path.js
+++ src/shapes/path.js
@@ -37,7 +37,7 @@ export const Interface = ({ layer, path, fromMask }) => {
     return <Wrapper layer={layer} path={path} name='Path' withVisibility>
         <Warning title={"Proceed With Caution"}>
             <p>This feature is a bit closer to the "bare metal", so might be more difficult to use. The below field expects the contents of the 'd' attribute of SVG's 'path' tag; read more at <a href='https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Paths' target='_blank' rel='noreferrer'>MDN</a> about SVG path commands.</p>
-            <p>This path is dependant coordinate system of the canvas. For your reference, know that the canvas's viewBox is '<code className='highlight selectable'>{`${w.value * w.unit / -2} ${h.value * h.unit / -2} ${w.value * w.unit} ${h.value * h.unit}`}</code>' and all points are drawn in that coordinate space.</p>
+            <p>This path is dependant on the coordinate system of the canvas. For your reference, know that the canvas's viewBox is '<code className='highlight selectable'>{`${w.value * w.unit / -2} ${h.value * h.unit / -2} ${w.value * w.unit} ${h.value * h.unit}`}</code>' and all points are drawn in that coordinate space.</p>
         </Warning>
         <Field label={"Definition"}>
             <BlockInput value={layer.definition} onChange={onChange(dispatch, `${path}.definition`)} />
