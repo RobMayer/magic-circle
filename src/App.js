96bb9f6550a6a768a9141a730ab2e546a26bbeee
RobMayer <5171361+RobMayer@users.noreply.github.com>
2021-05-16 21:14:37 -0400

    version bump

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { saveAs } from 'file-saver';
 import { validateUpload } from './util/validation';
 import Icon from './ui/icon';
 
-const version = "0.2.0";
+const version = "0.3.0";
 
 const handleUpload = (element, file, dispatch) => {
     if (file) {
